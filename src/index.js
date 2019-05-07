9d48b20bb8db64ff00a1ab83053c95913058973d
blacker <dongqingwen@lanjinrong.com>
2019-05-07 19:47:09 +0800

    webpack工程文件

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
+import createBrowserHistory from 'history/createBrowserHistory'
+import { createStore, createApp } from 'app'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './styles/index.scss'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const {store,history} =createStore(createBrowserHistory(),{})
+const app = createApp(store,history)
+
+ReactDOM.render(app,window.document.getElementById('app'))
