1f4c49b846f11e1af5f15b0b3835d883d9ed6c47
blacker <dongqingwen@lanjinrong.com>
2019-05-27 14:58:18 +0800

    项目运行成功

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom'
-import createBrowserHistory from 'history/createBrowserHistory'
-import { createStore, createApp } from 'app'
-
+import { createStore, createApp } from './app'
 import './styles/index.scss'
 
-const {store,history} =createStore(createBrowserHistory(),{})
-const app = createApp(store,history)
+const { createBrowserHistory } = require('history')
 
-ReactDOM.render(app,window.document.getElementById('app'))
+const { store, history } = createStore(createBrowserHistory(), {})
+const app = createApp(store, history)
+
+ReactDOM.render(app, window.document.getElementById('app'))
